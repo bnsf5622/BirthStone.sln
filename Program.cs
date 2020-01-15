af723d2fbc8e36ed0f03bec6b2345d66a0618aff
Chuck Francis <bnsf5622@gmail.com>
2020-01-14 19:07:12 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BirthStone.sln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
